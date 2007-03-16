31a2cd62a69f03958687ea1bd318987a2057eeb1
delgadop <>
2007-03-16 12:35:43 +0000

    Common for dls-* commands use (for the moment, error codes)

---
+++ Client/LFCClient/dlsCommandsCommon.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+#
+# $Id$
+#
+# DLS Client. $Name:  $.
+# Antonio Delgado Peris. CIEMAT. CMS.
+# 
+
+#########################################
+# Error codes
+#########################################
+# Error in the options or arguments parsing
+OPT_ERROR = 230
+# Error in the specified DLS type 
+TYPE_ERROR = 231
+# Error accessing a file specified as argument
+FILE_ERROR = 232
+# Caught API exception with no defined error code
+GENERIC_ERROR = 250
